d9c80fd3cafaa6933c451db5ff5f79e7cc630d1b
Sandrine Bailleux <sandrine.bailleux@arm.com>
2015-09-29 09:16:59 +0100

    arm-tf: Specify build target explicitly
    When building the Trusted Firmware sources, use 'make all' instead
    of relying on the default make target being 'all'.
    
    Signed-off-by: Sandrine Bailleux <sandrine.bailleux@arm.com>

--- build-arm-tf.sh
+++ build-arm-tf.sh
@@ -46,7 +46,7 @@ do_build ()
 	if [ "$ARM_TF_BUILD_ENABLED" == "1" ]; then
 		pushd $TOP_DIR/$ARM_TF_PATH
 		for plat in $ARM_TF_PLATS; do
-			local build_cmd="make -j $PARALLELISM PLAT=$plat DEBUG=$ARM_TF_DEBUG_ENABLED $ARM_TF_BUILD_FLAGS"
+			local build_cmd="make -j $PARALLELISM PLAT=$plat DEBUG=$ARM_TF_DEBUG_ENABLED $ARM_TF_BUILD_FLAGS all"
 			echo $build_cmd
 			$build_cmd
 		done
