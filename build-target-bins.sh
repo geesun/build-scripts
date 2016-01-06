deaa00f8ba86caa2c78e36e2c183f8c399f7333a
Ryan Harkin <ryan.harkin@linaro.org>
2016-01-06 14:23:13 +0000

    Update fip_create params for ARM-TF pull req 479
    While ARM-TF v1.2 changed the params of fip_create and cert_create as
    per commit "Fix for ARM-TF v1.2" (b7d86b9), pull request #479 [1]
    updates fip_create to use the same params as cert_create.
    
    This commit updated the script to match the new fip_create params.
    
    [1] https://github.com/ARM-software/arm-trusted-firmware/pull/479
    Signed-off-by: Ryan Harkin <ryan.harkin@linaro.org>

--- build-target-bins.sh
+++ build-target-bins.sh
@@ -223,30 +223,30 @@ do_package()
 			local scp_out="TARGET_SCP_"$target
 			local uboot_out="TARGET_UBOOT_"$target
 			local uefi_out="TARGET_UEFI_"$target
-			local bl2_fip_param="--bl2  ${OUTDIR}/${!tf_out}/tf-bl2.bin"
-			local bl31_fip_param="--bl31 ${OUTDIR}/${!tf_out}/tf-bl31.bin"
+			local bl2_fip_param="--tb-fw  ${OUTDIR}/${!tf_out}/tf-bl2.bin"
+			local bl31_fip_param="--soc-fw ${OUTDIR}/${!tf_out}/tf-bl31.bin"
 			local bl30_fip_param=
 			local bl32_fip_param=
 
 			if [ "${!scp_out}" != "" ]; then
-				bl30_fip_param="--bl30 ${TOP_DIR}/${!scp_out}/bl30.bin"
+				bl30_fip_param="--scp-fw ${TOP_DIR}/${!scp_out}/bl30.bin"
 			fi
 
 			if [ -e "${OUTDIR}/${!tf_out}/tf-bl32.bin" ]; then
-				bl32_fip_param="--bl32 ${OUTDIR}/${!tf_out}/tf-bl32.bin"
+				bl32_fip_param="--tos-fw ${OUTDIR}/${!tf_out}/tf-bl32.bin"
 			fi
 			local fip_param="${bl2_fip_param} ${bl31_fip_param}  ${bl30_fip_param} ${bl32_fip_param}"
 			if [ "$ARM_TBBR_ENABLED" == "1" ]; then
 				local trusted_key_cert_param="--trusted-key-cert ${OUTDIR}/${!tf_out}/trusted_key.crt"
-				local bl30_tbbr_param="--bl30-key-cert ${OUTDIR}/${!tf_out}/bl30_key.crt --bl30-cert ${OUTDIR}/${!tf_out}/bl30.crt"
-				local bl31_tbbr_param="--bl31-key-cert ${OUTDIR}/${!tf_out}/bl31_key.crt --bl31-cert ${OUTDIR}/${!tf_out}/bl31.crt"
+				local bl30_tbbr_param="--scp-fw-key-cert ${OUTDIR}/${!tf_out}/bl30_key.crt --scp-fw-cert ${OUTDIR}/${!tf_out}/bl30.crt"
+				local bl31_tbbr_param="--soc-fw-key-cert ${OUTDIR}/${!tf_out}/bl31_key.crt --soc-fw-cert ${OUTDIR}/${!tf_out}/bl31.crt"
 				local bl32_tbbr_param=
-				local bl33_tbbr_param="--bl33-key-cert ${OUTDIR}/${!tf_out}/bl33_key.crt --bl33-cert ${OUTDIR}/${!tf_out}/bl33.crt"
-				local bl2_tbbr_param="--bl2-cert ${OUTDIR}/${!tf_out}/bl2.crt"
+				local bl33_tbbr_param="--nt-fw-key-cert ${OUTDIR}/${!tf_out}/bl33_key.crt --nt-fw-cert ${OUTDIR}/${!tf_out}/bl33.crt"
+				local bl2_tbbr_param="--tb-fw-cert ${OUTDIR}/${!tf_out}/bl2.crt"
 
 				#only if a TEE implementation is available and built
 				if [ -e "${OUTDIR}/${!tf_out}/tf-bl32.bin" ]; then
-					bl32_tbbr_param="--bl32-key-cert ${OUTDIR}/${!tf_out}/bl32_key.crt --bl32-cert ${OUTDIR}/${!tf_out}/bl32.crt"
+					bl32_tbbr_param="--tos-fw-key-cert ${OUTDIR}/${!tf_out}/bl32_key.crt --tos-fw-cert ${OUTDIR}/${!tf_out}/bl32.crt"
 				fi
 				# add the cert related params to be used by fip_create as well as cert_create
 				fip_param="${fip_param} ${trusted_key_cert_param} \
@@ -269,13 +269,13 @@ do_package()
 				if [ "$ARM_TBBR_ENABLED" == "1" ]; then
 					$TOP_DIR/$ARM_TF_PATH/tools/cert_create/cert_create  \
 						${cert_tool_param} \
-						--bl33 ${OUTDIR}/${!uboot_out}/uboot.bin
+						--nt-fw ${OUTDIR}/${!uboot_out}/uboot.bin
 
 				fi
 				if [ "$ARM_TF_BUILD_ENABLED" == "1" ]; then
 					$TOP_DIR/$ARM_TF_PATH/tools/fip_create/fip_create --dump  \
 						${fip_param} \
-						--bl33 ${OUTDIR}/${!uboot_out}/uboot.bin \
+						--nt-fw ${OUTDIR}/${!uboot_out}/uboot.bin \
 						$outfile
 					cp ${OUTDIR}/${!tf_out}/tf-bl1.bin $outdir/bl1.bin
 				else
@@ -293,13 +293,13 @@ do_package()
 				if [ "$ARM_TBBR_ENABLED" == "1" ]; then
 					$TOP_DIR/$ARM_TF_PATH/tools/cert_create/cert_create  \
 						${cert_tool_param} \
-						--bl33 ${OUTDIR}/${!uefi_out}/uefi.bin
+						--nt-fw ${OUTDIR}/${!uefi_out}/uefi.bin
 
 				fi
 				if [ "$ARM_TF_BUILD_ENABLED" == "1" ]; then
 					$TOP_DIR/$ARM_TF_PATH/tools/fip_create/fip_create --dump  \
 						${fip_param} \
-						--bl33 ${OUTDIR}/${!uefi_out}/uefi.bin \
+						--nt-fw ${OUTDIR}/${!uefi_out}/uefi.bin \
 						$outfile
 					cp ${OUTDIR}/${!tf_out}/tf-bl1.bin $outdir/bl1.bin
 				else
