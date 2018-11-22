1b843ecdaaff4ac7e2e27b11ced8ffbee7eab390
Lokesh B V <lokesh.bv@arm.com>
2018-11-22 11:55:01 +0530

    build-sct: add a component build script to build sct package in edk2
    Add a component build script to support builidng of UEFI SCT package
    in edk2.
    
    Change-Id: If47384ab55fb29c7f35c0a6a3d4be6286a9ce74f
    Signed-off-by: Lokesh B V <lokesh.bv@arm.com>

---
+++ build-sct.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2019, ARM Limited and Contributors. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# Neither the name of ARM nor the names of its contributors may be used
+# to endorse or promote products derived from this software without specific
+# prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+do_build()
+{
+	pushd $TOP_DIR/$UEFI_PATH
+	if [[ ! -e ./SctPkg ]] ; then
+		ln -s edk2-test/uefi-sct/SctPkg SctPkg
+	fi
+	echo Building UEFI SCT...
+	./SctPkg/build.sh AARCH64 GCC
+	popd
+}
+
+do_package()
+{
+	mkdir -p $SCT_OUT_DIR
+	cp -rf $TOP_DIR/$UEFI_PATH/Build/UefiSct/DEBUG_GCC49/SctPackageAARCH64 $SCT_OUT_DIR
+}
+
+do_clean()
+{
+	rm -rf $SCT_OUT_DIR
+	rm -rf $TOP_DIR/$UEFI_PATH/Build/*Sct
+}
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source $DIR/framework.sh $@
