1ac26a1251eb5b498c224b93e9f6371758847f75
Omkar Anand Kulkarni <omkar.kulkarni@arm.com>
2020-04-22 13:23:50 +0530

    build-uefi.sh: pass compile time defined macros correctly to the build
    The compile time macros defined the platform configuration file for
    UEFI build is not correctly passed as build parameters during edk2
    build. Fix this.
    
    Change-Id: I7de41c754a402c66f86bbfef6b5c893104aa5694
    Signed-off-by: Omkar Anand Kulkarni <omkar.kulkarni@arm.com>

--- build-uefi.sh
+++ build-uefi.sh
@@ -73,16 +73,11 @@ do_build ()
 				IASL_PREFIX=${TOP_DIR}/${UEFI_ACPICA_PATH}/bin/ make -f ${!makefile} EDK2_BUILD=$UEFI_BUILD_MODE
 			else
 				outdir=UEFI_PLAT_$item[platname]
+				vars=UEFI_PLAT_$item[defines]
 				echo
+				echo "EDK2 build parameters: " $UEFI_EXTRA_BUILD_PARAMS ${!vars}
 				echo
-				echo
-				echo
-				echo $UEFI_EXTRA_BUILD_PARAMS
-				echo
-				echo
-				echo
-				echo
-				EXTRA_OPTIONS="-s" IASL_PREFIX=${TOP_DIR}/${UEFI_ACPICA_PATH}/bin/ ${TOP_DIR}/${UEFI_TOOLS_PATH}/edk2-build.sh -e ${TOP_DIR}/uefi/edk2 -p ${TOP_DIR}/uefi/edk2/edk2-platforms -D EDK2_OUT_DIR=Build/${!outdir} $UEFI_EXTRA_BUILD_PARAMS -b $UEFI_BUILD_MODE $item
+				EXTRA_OPTIONS="-s" IASL_PREFIX=${TOP_DIR}/${UEFI_ACPICA_PATH}/bin/ ${TOP_DIR}/${UEFI_TOOLS_PATH}/edk2-build.sh -e ${TOP_DIR}/uefi/edk2 -p ${TOP_DIR}/uefi/edk2/edk2-platforms -D EDK2_OUT_DIR=Build/${!outdir} $UEFI_EXTRA_BUILD_PARAMS ${!vars} -b $UEFI_BUILD_MODE $item
 			fi
 		done
 		popd
