90fd09bb420c4250b7992dd4eccec2ed670e574e
Zakaria Zahi <zakaria.zahi@arm.com>
2021-08-23 02:01:36 +0530

    sgi/install_prerequisites.sh: install 'cmake' using pip
    CMake package provided through apt repository is not compatible with the
    SCP firmware's CMake requirements. So install the latest version of
    CMake using Package installer for Python (pip).
    
    Signed-off-by: Zakaria Zahi <zakaria.zahi@arm.com>
    Change-Id: Ib9e80456f5706fed2f5418f83b4a7d7927a69fe8

--- sgi/install_prerequisites.sh
+++ sgi/install_prerequisites.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright (c) 2019, ARM Limited and Contributors. All rights reserved.
+# Copyright (c) 2019-2021, ARM Limited and Contributors. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -79,7 +79,6 @@ APT_PACKAGES_COMMON=(
 		"autopoint"
 		"doxygen"
 		"xterm"
-		"cmake"
 		"ninja-build"
 )
 
@@ -222,6 +221,10 @@ ${NORMAL} ${BOLD}${BLUE}`date`${NORMAL}\n"
 
 	echo -e "\nInstalling Required Packages:\n\n"
 	install_package
+
+	echo -e "\n Installing CMake - \n\n"
+	pip install cmake --upgrade >> $LOGFILE 2>&1
+	echo -e "${BOLD}${GREEN}done${NORMAL}"
 
 	echo -e "\nInstalltion of prerequisites ended at \
 ${NORMAL} ${BOLD}${BLUE}`date`${NORMAL}\n"
